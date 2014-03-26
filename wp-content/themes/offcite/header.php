ab81f8f55951918620d85f7e35d76a32f4a3d2b1
Andrew Cobb <cobb.andrew@gmail.com>
2014-03-26 12:33:55 -0700

    CSS for embedded iframes and file perm modifications.

--- wp-content/themes/offcite/header.php
+++ wp-content/themes/offcite/header.php
@@ -3,20 +3,20 @@
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
 <title><?php bloginfo( 'name' ); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
-<link rel="stylesheet" href="http://offcite.org/wp-content/themes/offcite/style.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="http://offcite.org/wp-content/themes/offcite/css/print.css" type="text/css" media="print" />  
+<link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/offcite/style.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/offcite/css/print.css" type="text/css" media="print" />  
 <!--[if IE]>
-<link rel="stylesheet" href="http://offcite.org/wp-content/themes/offcite/css/ie.css" type="text/css" media="screen, projection" />
+<link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/offcite/css/ie.css" type="text/css" media="screen, projection" />
 <![endif]-->
 <!--[if lte IE 6]>
-<link rel="stylesheet" href="http://offcite.org/wp-content/themes/offcite/css/ie6.css" type="text/css" media="screen, projection" />
+<link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/offcite/css/ie6.css" type="text/css" media="screen, projection" />
 <![endif]-->
 <script type="text/javascript">
 try {
   document.execCommand("BackgroundImageCache", false, true);
 } catch(err) {}
 </script>
-<link rel="pingback" href="http://offcite.org/xmlrpc.php" />
+<link rel="pingback" href="<?php echo get_site_url(); ?>/xmlrpc.php" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -29,10 +29,10 @@ try {
 					wp_nav_menu( 'theme_location=primary&container=false&menu_id=headernavigation' );
 				} else { ?>
 				 <ul id="headernavigation">
-					<li id="feeds"><a href="http://offcite.org/feeds" title="Offcite Feeds">Feeds</a></li>
-					<li id="contact"><a href="http://offcite.org/contact" title="Contact Offcite">Contact</a></li>
-					<li id="about"><a href="http://offcite.org/about" title="About Offcite">About</a></li>
+					<li id="feeds"><a href="<?php echo get_site_url(); ?>/feeds" title="Offcite Feeds">Feeds</a></li>
+					<li id="contact"><a href="<?php echo get_site_url(); ?>/contact" title="Contact Offcite">Contact</a></li>
+					<li id="about"><a href="<?php echo get_site_url(); ?>/about" title="About Offcite">About</a></li>
 				</ul>
 			<?php } ?>
-            <h1 id="logo"><a href="http://offcite.org/" title="Offcite Blog | Design.  Houston.  Architecure.">Offcite Blog | Design.  Houston.  Architecure.</a></h1>
+            <h1 id="logo"><a href="<?php echo get_site_url(); ?>/" title="Offcite Blog | Design.  Houston.  Architecure.">Offcite Blog | Design.  Houston.  Architecure.</a></h1>
         </div><!--end header div-->
