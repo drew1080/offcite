87760fb8b8cd1169293b73f227986f88109f0909
Andrew Cobb <cobb.andrew@gmail.com>
2014-04-21 10:34:05 -0500

    Add featured image support for single blog post page.

--- wp-content/themes/offcite-2014/single.php
+++ wp-content/themes/offcite-2014/single.php
@@ -1,30 +1,58 @@
 <?php get_header(); ?>
 
+
+
         <div id="content">       
+
         
-    <?php if (have_posts()) : ?>
+
+    <?php if (have_posts()) : 
 	
-    
-	<?php //Prepare to check if this is the topmost Post on the page
+	if ( has_category( 'unexpected-city' ) ) : ?>
+		<img src="http://offcite.org/wp-content/themes/offcite/images/unexpected-city-badge.png" class="unexpected-cities-badge" height="26" width="187" />
+	<?php endif;
+	
+	
+	 //Prepare to check if this is the topmost Post on the page
+
 		  $firstpost = 'true'; ?>
 
+
+
 		<?php while (have_posts()) : the_post(); 
+
 		$allpostcontent = $post ->post_content;
+
 		$regex = '#<!--\s*featured\s*-->(.*?)<!--\s*endfeatured\s*-->#s';	
+
 		preg_match_all( $regex, $allpostcontent, $fpics );
+
 		$numpics = count ($fpics[0]); ?>
+
           
+
         <?php //Check to see if this is the topmost Post on the page
+
 			if($firstpost == 'true' ) { ?>
+
             <div class="post firstpost" id="post-<?php the_ID(); ?>">
+
             <?php $firstpost = 'false'; ?>
+
         <?php } else { ?>
+
         	<div class="post" id="post-<?php the_ID(); ?>">  
+
         <?php } ?>
+
             
+
             
-        <?php if ( $numpics > 0 || has_post_thumbnail()) { ?> 
+
+        <?php if ( $numpics > 0 || has_post_thumbnail() ) { ?> 
+
         <div class="featuredphoto"> 
+
 		<?php 
     // Check for featured image, if not there, use the old featured comment method
     // This will allow for legacy posts to function properly
@@ -36,112 +64,140 @@
       }
     }; 
     ?>
+
 		</div><!--end featuredphoto div-->
+
 		<?php } else { ?>
+
         <div class="nofeatured"></div>
+
         <?php }; ?>
+
                     
+
             <div class="authormeta">
+
+
             <ul>
+
                 <li class="authorname"><a href="http://offcite.org/about" title="About OffCite"><?php the_author(); ?></a></li>
+
                 <li><?php the_time( 'M. j, Y' ); ?></li>
+
                 <li><?php the_time( 'g:i A' ); ?></li>
+
             </ul>
+
             </div><!--end authormeta div-->
+
             
-            <div class="excerpt">
+
+            <div class="excerpt entry">
+
             <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+            
+
             <ul class="excerptmeta">
-                <?php if ( comments_open() ) : ?>
+
+            	 <?php if ( comments_open() ) : ?>
+
                 <li><strong><?php the_category( ', ' ); ?></strong></li>
-                <li>Comments <span><?php comments_popup_link( '0', '1', '%' ); ?></span></li>
-                <?php endif; ?>
+
+                <li>Comments <span><a href="#respond" title="Write Your Response"><?php comments_number( '0', '1', '%' ); ?></a></span></li>		<?php endif; ?>
+
                 <?php if ( !comments_open() ) : ?>
+
                 <li><strong style="border: none;"><?php the_category( ', ' ); ?></strong></li>
-                <?php endif; ?>
+
+                <?php endif; ?>                
+
             </ul>
+
             
+
+            
+
+            
+
       <?php // Outputting the content, minus the first image in the post
+
 	  // Additionally, only content above the <!--more--> tag will be shown
+
       $offcitecontent = preg_replace( $regex, '' , $allpostcontent);
-	  $pieces = explode( '<!--more-->',$offcitecontent);	  
-      $offcitecontent = apply_filters( 'the_content', $pieces[0]); 
-      echo $offcitecontent;              
-      ?>
-			<p><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">more &gt;</a></p>
-            <?php edit_post_link( 'edit', '<p>', '</p>' ); ?>
+
+      $offcitecontent = apply_filters( 'the_content', $offcitecontent); 
+
+      echo $offcitecontent;
+	  
+	  wp_link_pages();          
+
+      edit_post_link( 'edit', '<p>', '</p>' )
+
+	  //tags ?>
+
+      
+
+      <p class="thetags"><?php if(function_exists( 'wp_email' )) { email_link(); } ?> <?php the_tags( 'Filed Under: ',', ' ); ?></p>
+
+      
+
+      <?php 
+
+      //sociable links
+
+	  if (function_exists( 'sociable_html' )) { echo sociable_html(); } ?>
+
+            <?php comments_template(); ?>
+
             </div>
+
             
+
             <hr class="space"/>
+
             </div><!--end post div-->
+
 			
 
+
+
 		<?php endwhile; ?>
+
             
+
 </div><!--end content div-->
 
-<?php //prev/nav links
-//get the current page number
-$page_num = $paged;
-if($page_num == '' ){$page_num = '1';}
-//get the number of the last page
-$max_num_pages = $wp_query->max_num_pages;
 
-//first and only page
-if( $page_num == 1 && $max_num_pages==$page_num ){ ?>
+
 <div id="footer">
-    <ul>
-        <li id="olderposts"><a class="disabled" href="#">Older Posts</a></li>
-        <li id="foothome"><a href="http://offcite.org/" title="Home">Home</a></li>
-        <li id="newerposts"><a class="disabled" href="#">Newer Posts</a></li>
-    </ul>
-<?php get_footer(); ?>
-</div>
-<?php }
 
-//first of many pages
-if( $page_num == 1 && $max_num_pages>$page_num ){ ?>
-<div id="footer">
     <ul>
-        <li id="olderposts"><?php next_posts_link( 'Older Posts' ); ?></li>
-        <li id="foothome"><a href="http://offcite.org/" title="Home">Home</a></li>
-        <li id="newerposts"><a class="disabled" href="#">Newer Posts</a></li>
-    </ul>
-<?php get_footer(); ?>
-</div>
-<?php }
 
+        <?php previous_post_link( '<li id="olderposts">%link</li>', 'older posts' ); ?>
 
-//middle page
-if( $page_num > 1 && $max_num_pages>$page_num ){ ?>
-<div id="footer">
-    <ul>
-        <li id="olderposts"><?php next_posts_link( 'Older Posts' ); ?></li>
         <li id="foothome"><a href="http://offcite.org/" title="Home">Home</a></li>
-        <li id="newerposts"><?php previous_posts_link( 'Newer Posts' ); ?></li>
-    </ul>
-<?php get_footer(); ?>
-</div>
-<?php }
 
-//last page
-if( $page_num == $max_num_pages && $max_num_pages > 1 ){ ?>
-<div id="footer">
-    <ul>
-        <li id="olderposts"><a class="disabled" href="#">Older Posts</a></li>
-        <li id="foothome"><a href="http://offcite.org/" title="Home">Home</a></li>
-        <li id="newerposts"><?php previous_posts_link( 'Newer Posts' ); ?></li>
+        <?php next_post_link( '<li id="newerposts">%link</li>', 'newer posts' ); ?>
+
     </ul>
+
+    
 
 <?php get_footer(); ?>
 
+
+
 </div>
-<?php } ?>
 
 
-<?php else : ?>
 
-<?php  endif; ?>
+	<?php else : ?>
+
+	<?php endif; ?>
+
+
 
 </div><!--end center div-->
+
 <?php get_sidebar(); ?>
